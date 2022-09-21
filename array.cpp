e4eacad5cecf474fa0b1e4f9db68b26331d67602
Abhay Chaudhary <113006425+chdry128@users.noreply.github.com>
2022-09-21 12:07:24 +0530

    lab projects.

---
+++ array.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x[10];
+	for(int i=0;i<10;i++)
+{
+		
+	cout<<"enter the data";
+     cin>>x[i];
+
+}
+for(i=0;i<10;i++)
+ {
+     cout<<x[i];
+ }
+}
---
+++ arrayname.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char name[15];
+	cout<<"enter your name";
+	cin>>name;
+	cout<<"your name is "<<name;
+	return  0;
+}
