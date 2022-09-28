8a0f7f0d600db79f9b81594faf70e08929a48045
Abhay Chaudhary <113006425+chdry128@users.noreply.github.com>
2022-09-28 11:41:42 +0530

    Update AbhayReversed.cpp

--- AbhayReversed.cpp
+++ AbhayReversed.cpp
@@ -6,7 +6,7 @@
 	int len = strlen(str);
 	for (int i =0;i<len;i++)
 	{
-	 std::cout<<str[i]<<i[str]<<*(str+i)<<*(i+str);
+	 std::cout<<str[i]<<i[str]<<*(str+i)<<*(i+str)<<"\n";
 		
 	}
 	int lenM = len/2;
@@ -18,7 +18,7 @@
 		str[i] = str[i]- str[len-i];
 		
 	}
-	 std::cout<<"the string reversed is"<<str;
+	 std::cout<<"the string reversed is \t:"<<str;
 	
 	return 0;
 	
