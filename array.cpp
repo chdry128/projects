788f25792ee17c4f62a40f092be9e033e04d8003
Abhay Chaudhary <113006425+chdry128@users.noreply.github.com>
2022-11-18 12:09:24 +0530

    Add files via upload
    c program to add a elemet in the array in the position entered by the user.

--- array.cpp
+++ array.cpp
@@ -1,17 +1,18 @@
-#include<iostream>
-using namespace std;
+/* WAP to inisert an element in array at position entered by the user */
+#include<stdio.h>
 int main()
-{
-	int x[10];
-	for(int i=0;i<10;i++)
-{
-		
-	cout<<"enter the data";
-     cin>>x[i];
+{int pos,i,n;
 
-}
-for(i=0;i<10;i++)
- {
-     cout<<x[i];
- }
+	int a[]={1,2,3,4,5};
+	printf("enter the position u want to enter the element");
+	scanf("%d",&pos);
+	printf("enter the element ");
+	scanf("%d",& n);
+	a[pos]=n;
+	for(i=0;i<5;i++)
+	{
+		printf("%d",a[i]);
+		
+	}
+	
 }
